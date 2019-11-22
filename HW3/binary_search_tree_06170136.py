81c37697d52f88a384e4ae41884c1894e3ab95cf
Juicy <55619645+jason-28@users.noreply.github.com>
2019-11-22 11:59:18 +0800

    Update binary_search_tree_06170136.py

--- HW3/binary_search_tree_06170136.py
+++ HW3/binary_search_tree_06170136.py
@@ -35,5 +35,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify nodes(maybe more than more) in-place instead.(cannot search())
         """
 """
-# 參考網址:https://www.google.com/url?q=https://youtu.be/YlgPi75hIBc&sa=D&ust=1573899284113000&usg=AFQjCNEeK-0kWFD7MSL8bB85iUq0EM_4Pw
-  
+參考網址:https://www.google.com/url?q=https://youtu.be/YlgPi75hIBc&sa=D&ust=1573899284113000&usg=AFQjCNEeK-0kWFD7MSL8bB85iUq0EM_4Pw
+
+https://github.com/joeyajames/Python/blob/master/Trees/BinarySearchTree.py
+"""  

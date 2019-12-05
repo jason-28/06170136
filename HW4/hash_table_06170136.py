e1186dbdefb7716060efabaa432065b71953cd58
Juicy <55619645+jason-28@users.noreply.github.com>
2019-12-06 02:23:40 +0800

    Update hash_table_06170136.py

--- HW4/hash_table_06170136.py
+++ HW4/hash_table_06170136.py
@@ -1,3 +1,56 @@
+from Crypto.Hash import MD5
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+      
+    
+class MyHashSet:
+    def __init__(self, capacity=5):
+        self.capacity = capacity
+        self.data = [None] * capacity
+     
+    def hexdigest(self,str): 
+        h=MD5.new() 
+        h.update(str.encode("utf-8")) 
+        num=int(h.hexdigest(),16) 
+        return num 
+    
+    def add(self, key):
+        num=self.hexdigest(key)
+        i=num%self.capacity
+        cur=self.data[i]
+        new=ListNode(num)
+        new.next=self.data[i]
+        self.data[i]=new
+        
+        while cur!=None:
+            if cur.val==num:
+                pass
+            cur=cur.next
+            
+    def remove(self, key):
+        num=self.hexdigest(key)
+        i=num%self.capacity
+        cur=self.data[i]
+        
+        while cur!=None:
+            if cur.val==num:
+                self.data[i]=cur.next
+                return self.data[i]
+            else:
+                cur=cur.next
+            return
+        
+    def contains(self, key):
+        num=self.hexdigest(key)
+        i=num%self.capacity
+        cur=self.data[i]
+        while cur!=None:
+            if cur.val==num:
+                return True
+            cur=cur.next
+        return False
 """
 參考網址:https://leetcode.com/problems/design-hashset/discuss/347478/Python-Solution-with-Chaining-without-boolean-array-(Explanation-included)
 https://kite.com/python/examples/2084/crypto-generate-a-new-md5-hash

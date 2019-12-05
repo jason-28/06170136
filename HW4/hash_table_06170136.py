00fa0ac18e33effec29a75aeadf08548ce7746bc
Juicy <55619645+jason-28@users.noreply.github.com>
2019-12-06 01:58:01 +0800

    Update hash_table_06170136.py

--- HW4/hash_table_06170136.py
+++ HW4/hash_table_06170136.py
@@ -1,1 +1,5 @@
+"""
+參考網址:https://leetcode.com/problems/design-hashset/discuss/347478/Python-Solution-with-Chaining-without-boolean-array-(Explanation-included)
+https://kite.com/python/examples/2084/crypto-generate-a-new-md5-hash
+"""  
 

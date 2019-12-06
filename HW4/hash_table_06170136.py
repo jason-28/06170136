dedf850b32bdd8d116e929cf281676ff77d3649b
Juicy <55619645+jason-28@users.noreply.github.com>
2019-12-06 19:05:23 +0800

    Update hash_table_06170136.py

--- HW4/hash_table_06170136.py
+++ HW4/hash_table_06170136.py
@@ -51,8 +51,3 @@ class MyHashSet:
                 return True
             cur=cur.next
         return False
-"""
-參考網址:https://leetcode.com/problems/design-hashset/discuss/347478/Python-Solution-with-Chaining-without-boolean-array-(Explanation-included)
-https://kite.com/python/examples/2084/crypto-generate-a-new-md5-hash
-"""  
-

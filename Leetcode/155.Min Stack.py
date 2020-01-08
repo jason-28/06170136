f6e8149fbacbabab1afed5c881c63e26fa42e448
Juicy <55619645+jason-28@users.noreply.github.com>
2020-01-09 00:23:09 +0800

    Update and rename 155Min Stack.py to 155.Min Stack.py

--- Leetcode/155.Min Stack.py
+++ Leetcode/155.Min Stack.py
@@ -1,9 +1,7 @@
 class MinStack:
 
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
+    
         self.list=[]
         self.len=0
         
@@ -13,11 +11,11 @@ class MinStack:
         self.len+=1
 
     def pop(self) -> None:
-        if self.len==0:
-            pass
-        else:
+        if self.len!=0:
             self.list.pop(self.len-1)
             self.len-=1
+        else:
+            pass
 
     def top(self) -> int:
         return self.list[self.len-1]
@@ -26,9 +24,3 @@ class MinStack:
         return min(self.list)
 
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
